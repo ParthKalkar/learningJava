81cba714b012ac467d1251f88f8746688cec6bda
ParthKalkar <parthkalkar111@gmail.com>
2023-06-22 18:06:01 +0300

    Java Course commit

---
+++ src/FirstJavaClass.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class FirstJavaClass {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+//        int age = 18;
+//        float averageMarks = 18.245f;
+//        char grade = 'A';
+//        String name = "Parth";
+//        boolean isValid = false;
+//        long bigNumber = 999999999999999999l;
+
+
+
+
+//        System.out.println(age);
+//        System.out.println(averageMarks);
+//        System.out.println(name);
+//        System.out.println(isValid);
+//        System.out.println(bigNumber);
+
+//        int firstNumber = 123;
+//        int secondNumber = 234;
+//
+//        int sum = firstNumber + secondNumber;
+//        System.out.println(sum);
+//
+//        int diff = firstNumber - secondNumber;
+//        System.out.println(diff);
+//
+//        int div = firstNumber / secondNumber;
+//        System.out.println(div);
+//
+//        int mult = firstNumber * secondNumber;
+//        System.out.println(mult);
+
+//        int a = 12;
+//        a++;
+//        System.out.println(a);
+//
+//        int b = 13;
+//        ++b;
+//        System.out.println(b);
+
+        // a++ updates the value in the next line
+        // ++a updates the value in the same line
+
+        // taking user input
+        Scanner sc = new Scanner((System.in));
+        System.out.println("Enter your age:");
+
+        // primitive data types are the one where the
+        // java has already defined the value
+
+        int age = sc.nextInt();
+        float grade = sc.nextFloat();
+
+        System.out.println(grade/10);
+
+        System.out.println(age * 2);
+
+
+
+
+
+
+    }
+}
